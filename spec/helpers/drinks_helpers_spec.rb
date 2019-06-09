0bf087c39d09335cc9559b90695d57fe5b98428f
Felipe Eleoterio <felipe.eleoterio@gmail.com>
2019-06-09 17:14:02 -0300

    teste helper metodos origin base_ingredients

--- spec/helpers/drinks_helpers_spec.rb
+++ spec/helpers/drinks_helpers_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 describe DrinksHelper do
   describe 'Validations Helper' do
-    subject { drink }
 
-    let(:drink) { FactoryBot.build :drink }
+    it "method base_ingredient" do
+      base_db = Drink.select(:base_ingredient).group(:base_ingredient).order(:base_ingredient)
+      base_helper = DrinksHelper.base_ingredient
+      expect(base_helper).to eq(base_db)
+    end
+
+    it "method origin" do
+      origin_db = Drink.select(:origin).group(:origin).order(:origin)
+      origin_helper = DrinksHelper.origin
+      expect(origin_helper).to eq(origin_db)
+    end
 
     it "method format_pages 0" do
       pages = DrinksHelper.format_pages(1)
